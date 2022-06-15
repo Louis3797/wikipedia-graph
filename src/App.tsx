afbfb3e7f5826ff5f6344b51170b3c20ac2bff30
Louis <louis-kaan2002@web.de>
2022-06-15 15:54:47 +0200

    added postprocessing to the nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignpre
 import { useEffect, useRef, useState, useCallback } from "react";
 import { ForceGraph3D } from "react-force-graph";
 import { API_PREFIX, API_SUFFIX } from "./utils/constants";
 import generateColorHex from "./utils/generateHexColor";
-
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
 interface Node {
   title: string;
   color: string;
@@ -49,7 +48,7 @@ function App() {
     depth -= 1;
 
     const url = API_PREFIX + title + API_SUFFIX;
-    fetch(url)
+    fetch(url, { cache: "force-cache" })
       .then((response) => response.json())
       .then((response) => {
         const pages = response.query.pages;
@@ -77,7 +76,7 @@ function App() {
         }
       })
       .catch((error) => {
-        throw new Error(error);
+        console.error(error);
       });
   }
 
@@ -91,23 +90,36 @@ function App() {
     };
 
     setNodes((n) => [...n, firstNode]);
-    getData("Albert Einstein", 2, firstNode);
+
+    const bloomPass: any = new UnrealBloomPass();
+    bloomPass.strength = 1.5;
+    bloomPass.radius = 1;
+    bloomPass.threshold = 0.1;
+    fgRef.current.postProcessingComposer().addPass(bloomPass);
+    fgRef.current.d3Force("charge").strength(-200);
+
+    getData("Albert Einstein", 5, firstNode);
   }, []);
 
   return (
     <div className="h-full w-screen">
       <ForceGraph3D
         graphData={{ nodes, links }}
-        linkWidth={1}
+        linkWidth={2}
         // @ts-ignore
-        nodeAutoColorBy={(n) => n.color}
-        linkDirectionalParticles={2}
+        nodeAutoColorBy="group"
+        // linkDirectionalParticles={2}
         ref={fgRef}
         onNodeClick={handleClick}
         // @ts-ignore
         nodeLabel={(n) => `${n.title}`}
+        nodeVal={40}
         warmupTicks={100}
         cooldownTicks={0}
+        linkColor="#CDF0EA"
+        linkOpacity={0.5}
+        linkCurvature={Math.random()}
+        linkCurveRotation={Math.random() * 360}
         // onEngineStop={() => fgRef.current.zoomToFit(400)}
       />
       <p className="absolute top-3 right-5 text-lg text-gray-100">Nodes: {nodes.length}</p>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const API_PREFIX =
   "https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&titles=";
-export const API_SUFFIX = "&prop=links&pllimit=3";
+export const API_SUFFIX = "&prop=links&pllimit=5";
